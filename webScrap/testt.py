77a02e15b283a5e264e44b6b92eda8f755b6ce58
vishal <vishalbabu.in@gmail.com>
2018-05-13 18:58:56 +0530

    web scrap

---
+++ webScrap/testt.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  9 11:37:59 2018
+
+@author: 10639497
+"""
+
+import os
+import tqdm
+
+path  = "D:\\Vishal\\DataSets\\HE\\DeepLerning2\\"
+
+train_path = path +"train_"
+
+def create_training_data():
+    training_data = []
+    for i in 
